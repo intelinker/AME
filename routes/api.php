10c6f52ddffd5a612d0c506a9fe0ab2f5301d5ea
YuanGuan <intelinker@163.com>
2017-04-22 02:46:11 +0800

    api vi route

--- routes/api.php
+++ routes/api.php
@@ -17,23 +17,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix'=>'v1'], function() {
-    Route::resource('article', 'ApiArticleController');
-    Route::resource('user', 'ApiUserController');
-    Route::get('mailcaptcha', 'ApiUserController@mailCaptcha');
-    Route::post('login', 'ApiUserController@signin');
-    Route::post('setavatar', 'ApiUserController@setavatar');
-    Route::post('setcover', 'ApiUserController@setcover');
-    Route::post('selfintro', 'ApiUserController@selfintro');
-    Route::post('maininfo', 'ApiSetupController@maininfo');
-    Route::get('loadtitles', 'ApiSetupController@loadTitles');
-    Route::get('loadpositions', 'ApiSetupController@loadPositions');
-    Route::get('loadcountries', 'ApiSetupController@loadCountries');
-    Route::post('personalinfo', 'ApiSetupController@personalInfo');
-    Route::post('aoginfo', 'ApiSetupController@aogInfo');
-    Route::post('privacyinfo', 'ApiSetupController@privacyInfo');
-    Route::get('acitivities', 'ApiArticleController@followingActivities');
-    Route::get('articlecomments/{$articleid}/{$page}/{$lastid}', 'ApiArticleController@loadComments');
-
-
-});
+//Route::group(['prefix'=>'v1'], function() {
+//    Route::resource('article', 'ApiArticleController');
+//    Route::resource('user', 'ApiUserController');
+//    Route::get('mailcaptcha', 'ApiUserController@mailCaptcha');
+//    Route::post('login', 'ApiUserController@signin');
+//    Route::post('setavatar', 'ApiUserController@setavatar');
+//    Route::post('setcover', 'ApiUserController@setcover');
+//    Route::post('selfintro', 'ApiUserController@selfintro');
+//    Route::post('maininfo', 'ApiSetupController@maininfo');
+//    Route::get('loadtitles', 'ApiSetupController@loadTitles');
+//    Route::get('loadpositions', 'ApiSetupController@loadPositions');
+//    Route::get('loadcountries', 'ApiSetupController@loadCountries');
+//    Route::post('personalinfo', 'ApiSetupController@personalInfo');
+//    Route::post('aoginfo', 'ApiSetupController@aogInfo');
+//    Route::post('privacyinfo', 'ApiSetupController@privacyInfo');
+//    Route::get('acitivities', 'ApiArticleController@followingActivities');
+//    Route::get('articlecomments/{$articleid}/{$page}/{$lastid}', 'ApiArticleController@loadComments');
+//
+//
+//});
--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,23 @@ Route::get('/', function () {
 
 Route::resource('article', 'ArticleController');
 
-//Route::group(['prefix'=>'article'], function(){
-//});
+Route::group(['prefix'=>'api/v1'], function() {
+    Route::resource('article', 'ApiArticleController');
+    Route::resource('user', 'ApiUserController');
+    Route::get('mailcaptcha', 'ApiUserController@mailCaptcha');
+    Route::post('login', 'ApiUserController@signin');
+    Route::post('setavatar', 'ApiUserController@setavatar');
+    Route::post('setcover', 'ApiUserController@setcover');
+    Route::post('selfintro', 'ApiUserController@selfintro');
+    Route::post('maininfo', 'ApiSetupController@maininfo');
+    Route::get('loadtitles', 'ApiSetupController@loadTitles');
+    Route::get('loadpositions', 'ApiSetupController@loadPositions');
+    Route::get('loadcountries', 'ApiSetupController@loadCountries');
+    Route::post('personalinfo', 'ApiSetupController@personalInfo');
+    Route::post('aoginfo', 'ApiSetupController@aogInfo');
+    Route::post('privacyinfo', 'ApiSetupController@privacyInfo');
+    Route::get('acitivities', 'ApiArticleController@followingActivities');
+    Route::get('articlecomments/{$articleid}/{$page}/{$lastid}', 'ApiArticleController@loadComments');
+
+
+});
